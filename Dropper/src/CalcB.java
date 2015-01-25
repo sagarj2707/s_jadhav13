12053768d0c743331d5491779b67156668857d59
Sagar Jadhav <sagarj2707@gmail.com>
2015-01-25 17:38:09 +0100

    25 Jan

---
+++ Dropper/src/CalcB.java
@@ -0,0 +1,17 @@
+public class CalcB {
+
+	double b, a = 4.0;
+	double n = 10.0;
+
+	public double CalcB() {
+
+		for (double k = 0; k <= n - 1; k++) {
+			double C = n - k;
+			b = (Math.pow(Math.E, (a * (C))));
+			
+		}
+		
+		return b;
+	}
+
+}
